50878dd80cccc92ae2efcba3e5b17e7d2bfbe045
tam74k <145953821+tam74k@users.noreply.github.com>
2026-08-19 23:55:33 +0300

    chore: update base path and favicon reference
    Change base path to '/office/' and update favicon path to root to ensure correct asset resolution in the production environment.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/office/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
